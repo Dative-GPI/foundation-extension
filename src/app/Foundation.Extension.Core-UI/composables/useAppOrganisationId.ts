6c0cb50a25a02f28257086fb63eab8ebd742ecf4
gballigand <gballigand@dative-gpi.com>
2024-07-15 14:41:20 +0200

    set shared organisationId

--- src/app/Foundation.Extension.Core-UI/composables/useAppOrganisationId.ts
+++ src/app/Foundation.Extension.Core-UI/composables/useAppOrganisationId.ts
@@ -1,13 +1,18 @@
 import { computed, ref } from "vue";
+import { useAppOrganisationId as useSharedOrganisationId } from "@dative-gpi/foundation-core-services/composables";
+
 import { setOrganisationId } from "../config/urls/urlFactory";
 
 const organisationId = ref<string | null>(null);
 const initialized = computed(() => (organisationId.value != null));
 
+const { setAppOrganisationId: setSharedOrganisationId } = useSharedOrganisationId();
+
 export const useAppOrganisationId = () => {
     const setAppOrganisationId = (payload: string) => {
         organisationId.value = payload;
         setOrganisationId(organisationId);
+        setSharedOrganisationId(organisationId.value);
     };
 
     return {

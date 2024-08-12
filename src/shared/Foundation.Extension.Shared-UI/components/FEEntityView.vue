3524026e377eb7947f9160f171663fb318893085
gballigand <gballigand@dative-gpi.com>
2024-08-12 15:23:35 +0200

    add FEEntityView

---
+++ src/shared/Foundation.Extension.Shared-UI/components/FEEntityView.vue
@@ -0,0 +1,40 @@
+<template>
+  <FSEntityViewUI
+    :imageSource="source"
+    v-bind="$attrs"
+  />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, type PropType } from "vue";
+
+import FSEntityViewUI from "@dative-gpi/foundation-shared-components/components/views/FSEntityViewUI.vue";
+import { useExtensionJwt } from "@dative-gpi/foundation-shared-services/composables";
+
+import { IMAGE_RAW_SOURCE_URL } from "../config";
+
+export default defineComponent({
+  name: "FEEntityView",
+  components: {
+    FSEntityViewUI
+  },
+  props: {
+    imageId: {
+      type: String as PropType<string | null>,
+      required: false,
+      default: null
+    }
+  },
+  setup(props) {
+    const { jwt } = useExtensionJwt();
+
+    const source = computed(() => {
+      return (props.imageId && jwt.value) ? IMAGE_RAW_SOURCE_URL(props.imageId, jwt.value) : null;
+    });
+
+    return {
+      source
+    };
+  }
+});
+</script>

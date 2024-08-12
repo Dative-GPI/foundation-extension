838f896aa83dab9ce96b2678903158b20bab8fed
gballigand <gballigand@dative-gpi.com>
2024-08-12 16:26:03 +0200

    add slot to feentityview

--- src/shared/Foundation.Extension.Shared-UI/components/FEEntityView.vue
+++ src/shared/Foundation.Extension.Shared-UI/components/FEEntityView.vue
@@ -2,7 +2,17 @@
   <FSEntityViewUI
     :imageSource="source"
     v-bind="$attrs"
-  />
+  >
+    <template
+      v-for="(_, name) in $slots"
+      v-slot:[name]="slotData"
+    >
+      <slot
+        :name="name"
+        v-bind="{ ...slotData }"
+      />
+    </template>
+  </FSEntityViewUI>
 </template>
 
 <script lang="ts">

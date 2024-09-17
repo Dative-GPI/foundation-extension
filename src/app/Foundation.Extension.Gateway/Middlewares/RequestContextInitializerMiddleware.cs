daee3b1b1c44342eecae46d8c25a8e3ed363438b
gballigand <balligandgm@gmail.com>
2024-09-17 16:08:42 +0200

    fix language code

--- src/app/Foundation.Extension.Gateway/Middlewares/RequestContextInitializerMiddleware.cs
+++ src/app/Foundation.Extension.Gateway/Middlewares/RequestContextInitializerMiddleware.cs
@@ -49,7 +49,20 @@ namespace Foundation.Extension.Gateway.Middlewares
                 sourceId = new Guid(request.Headers["X-Source-Id"].ToString());
 
             var applicationId = new Guid(request.Headers["X-Application-Id"].ToString());
-            var languageCode = request.Headers["X-Language-Code"].ToString();
+
+            string languageCode = null;
+            if (request.Headers.TryGetValue("X-Language-Code", out var applicationLanguageCode))
+            {
+                languageCode = applicationLanguageCode.ToString();
+            }
+            else if (request.Headers.TryGetValue("Accept-Language", out var browserLanguageCode))
+            {
+                languageCode = browserLanguageCode.ToString().Split(",")[0].Split(";")[0];
+            }
+            if (string.IsNullOrWhiteSpace(languageCode))
+            {
+                languageCode = "fr-FR";
+            }
 
             var isAuthenticated = request.Headers.ContainsKey(HeaderNames.Authorization);
             var jwt = isAuthenticated ? request.Headers[HeaderNames.Authorization].ToString().Substring(7) : null;

9b34079f5e43472874cf2da8b27ccc754143ec23
gballigand <balligandgm@gmail.com>
2024-11-04 16:29:56 +0100

    add foundation entity types

---
+++ src/shared/Foundation.Extension.Domain/FoundationEntityTypes.cs
@@ -0,0 +1,19 @@
+namespace Foundation.Extension.Domain
+{
+	public static class FoundationEntityTypes
+	{
+		public const string MODEL = "Model";
+		public const string GROUP = "Group";
+		public const string LOCATION = "Location";
+		public const string DEVICE = "Device";
+		public const string ALERT = "Alert";
+		public const string SCENARIO = "Scenario";
+		public const string USER = "User";
+		public const string SERVICE_ACCOUNT = "ServiceAccount";
+		public const string ROLE = "Role";
+		public const string DASHBOARD = "Dashboard";
+		public const string ORGANISATION = "Organisation";
+		public const string ORGANISATION_TYPE = "OrganisationType";
+		public const string ARTICLE = "Article";
+	}
+}

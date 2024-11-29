bbb722eb8e2fb132e543ef59587c9c585433bfed
gballigand <balligandgm@gmail.com>
2024-11-29 10:05:31 +0100

    complete translation provider

--- src/template/src/context/XXXXX.Context.Migrations.Shared/TranslationProvider.cs
+++ src/template/src/context/XXXXX.Context.Migrations.Shared/TranslationProvider.cs
@@ -16,18 +16,58 @@ namespace XXXXX.Context.Migrations.Shared
 			"../../../src/app/core/XXXXX.Core.UI",
 		};
 
-		public static async Task<List<Translation>> GetAllTranslations()
-		{
-			var translations = new List<Translation>();
+        public static async Task<List<Translation>> GetAllDistinctTranslations()
+        {
+            var translations = new List<Translation>();
 			var fixtureService = new FixtureService();
 
             foreach (var project in PROJECTS)
             {
-                var translation = await TranslationHelper.GetTranslations(project);
+                var translation = await TranslationHelper.GetDistinctTranslations(project);
                 translations.AddRange(translation);
             }
 
-			translations.AddRange(
+
+            var allTranslations = await GetTranslations(translations);
+
+			var foundationTranslationCodes = fixtureService.GetTranslations().Select(t => t.Code).ToList();
+
+            return allTranslations.DistinctBy(t => t.Code)
+				.Where(t => !foundationTranslationCodes.Contains(t.Code))
+                .ToList();
+        }
+
+        public static async Task<List<Translation>> GetAllTranslations()
+        {
+            var localTranslations = new List<Translation>();
+
+            foreach (var project in PROJECTS)
+            {
+                var translation = await TranslationHelper.GetTranslations(project);
+                localTranslations.AddRange(translation);
+            }
+
+            return await GetTranslations(localTranslations);
+        }
+
+        private static async Task<List<Translation>> GetTranslations(List<Translation> localTranslations)
+        {
+            var properties = await EntityPropertyProvider.GetAllEntityProperties();
+            var propertyTranslations = properties.Select(p =>
+            {
+                var isForeign = EntityPropertyHelper.IsForeignProperty(p.TranslationCode);
+                var heritedProperty = isForeign ? p.GetHeritedProperty(properties) : null;
+
+                return new Translation
+                {
+                    Code = p.TranslationCode,
+                    Value = p.LabelDefault ?? (isForeign && heritedProperty != null ? $"{heritedProperty.EntityType} {heritedProperty.LabelDefault}" : null),
+                    Context = isForeign && heritedProperty != null ? heritedProperty.Context : p.Context
+                };
+            });
+            
+            //Get all actions translations
+			localTranslations.AddRange(
                 XXXXX.Core.Kernel.Actions.GetActions()
                 .SelectMany(a => new List<Translation>()
                 {
@@ -40,7 +80,7 @@ namespace XXXXX.Context.Migrations.Shared
                 })
             );
 
-			translations.AddRange(
+			localTranslations.AddRange(
                 XXXXX.Admin.Kernel.Actions.GetActions()
                 .SelectMany(a => new List<Translation>()
                 {
@@ -53,12 +93,10 @@ namespace XXXXX.Context.Migrations.Shared
                 })
             );
 
-			var foundationTranslationCodes = fixtureService.GetTranslations().Select(t => t.Code).ToList();
-
-            return translations
-                .DistinctBy(t => t.Code)
-				.Where(t => !foundationTranslationCodes.Contains(t.Code))
+            //Concat propertyTranslations first to override existing translations if property context has been updated
+            return propertyTranslations
+                .Concat(localTranslations)
                 .ToList();
-		}
+        }
 	}
 }

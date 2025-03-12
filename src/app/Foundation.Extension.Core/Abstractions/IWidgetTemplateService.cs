d447466a45ec05dd69c158bea948fa2ba46d92ee
peyronth <peyron.th@gmail.com>
2025-03-12 10:34:29 +0100

    Fix injections

--- src/app/Foundation.Extension.Core/Abstractions/IWidgetTemplateService.cs
+++ src/app/Foundation.Extension.Core/Abstractions/IWidgetTemplateService.cs
@@ -7,7 +7,6 @@ namespace Foundation.Extension.Core.Abstractions
 {
 	public interface IWidgetTemplateService
     {
-        //Task<WidgetTemplateDetailsFoundationModel> Get(string languageCode, Guid widgetTemplateId);
         Task<IEnumerable<WidgetTemplateInfosFoundationModel>> GetMany(WidgetTemplatesFilterFoundationModel filter);
     }
 }
--- src/app/Foundation.Extension.Core/Services/WidgetTemplateService.cs
+++ src/app/Foundation.Extension.Core/Services/WidgetTemplateService.cs
@@ -17,35 +17,20 @@ namespace Foundation.Extension.Core.Services
     public class WidgetTemplateService : IWidgetTemplateService
     {
         private readonly RequestContext _requestContext;
-        private readonly IQueryHandler<WidgetTemplateQuery, WidgetTemplateDetails> _widgetTemplateQueryHandler;
         private readonly IQueryHandler<WidgetTemplatesQuery, IEnumerable<WidgetTemplateInfos>> _widgetTemplatesQueryHandler;
         private readonly IMapper _mapper;
 
         public WidgetTemplateService
         (
             IRequestContextProvider requestContextProvider,
-            IQueryHandler<WidgetTemplateQuery, WidgetTemplateDetails> widgetTemplateQueryHandler,
             IQueryHandler<WidgetTemplatesQuery, IEnumerable<WidgetTemplateInfos>> widgetTemplatesQueryHandler,
             IMapper mapper
         )
         {
             _requestContext = requestContextProvider.Context;
-            _widgetTemplateQueryHandler = widgetTemplateQueryHandler;
             _widgetTemplatesQueryHandler = widgetTemplatesQueryHandler;
             _mapper = mapper;
         }
-
-        /*public async Task<WidgetTemplateDetailsFoundationModel> Get(string languageCode, Guid widgetTemplateId)
-        {
-            var query = new WidgetTemplateQuery()
-            {
-                WidgetTemplateId = widgetTemplateId
-            };
-
-            var result = await _widgetTemplateQueryHandler.HandleAsync(query);
-
-            return _mapper.Map<WidgetTemplateDetails, WidgetTemplateDetailsFoundationModel>(result, opt => opt.Items.Add(Consts.LANGUAGE, languageCode));
-        }*/
 
         public async Task<IEnumerable<WidgetTemplateInfosFoundationModel>> GetMany(WidgetTemplatesFilterFoundationModel filter)
         {
